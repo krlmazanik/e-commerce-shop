70d27a8eaee0d9045f2be3b68fdc876dcc454256
Kiryl Mazanik <krlmazanik@gmail.com>
2021-09-26 14:22:35 +0200

    fix: update tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import App from './App';
+import { App } from './App';
 
-it('renders learn react link', () => {
-  mount(<App />);
-  cy.get('a').contains('Learn React');
+describe('App component', () => {
+  it('should render', () => {
+    mount(<App />);
+  });
 });
+
